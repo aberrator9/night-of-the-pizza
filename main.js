ee15890f83de36160da63997f0e4f35af8b70c73
Justin Linton <hello.aberrator@gmail.com>
2023-08-14 23:10:30 -0400

    Better generation with 2d array.

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
-const gameScreen = document.querySelector('.game-screen');
-const player = 'P';
-const wall = '';
-const smallDog = 'd';
-const empty = '.';
+const mapArea = document.querySelector('.map-area');
+const playerChar = 'P';
+const wallChar = '█';
+const streetChar = '▒';
+const smallDogChar = 'd';
+const emptyChar = '░';
+const houseChar = 'H';
+let house = [];
+let numHouses = between(5, 10);
 
-let map = [];
-let mapX = Math.floor(Math.random() * 40 + 20);
-let mapY = Math.floor(Math.random() * 40 + 20);
+let grid = [];
 
-gameScreen.innerHTML = generateMap();
-
-function generateMap() {
-    map = [];
-    for (let y = 0; y < mapY; ++y) {
-        let row = '';
-
-        if (y > 0 && y < mapY - 1) {
-            for (let x = 0; x < mapX; ++x) {
-                if (x > 0 && x < mapX - 1) {
-                    row += empty;
-                } else {
-                    row += wall;
-                }
-            }
-        } else {
-            row = wall.repeat(mapX);
-        }
-        map.push(row);
-    }
-
-    return map.join('\n');
+function between(min, max) {
+    return Math.floor(Math.random() * max + min);
 }
+
+function randomCoords(height, width) {
+    return [Math.floor(Math.random() * height), Math.floor(Math.random() * width)];
+}
+
+function createGrid(rows, cols) {
+    const grid = [];
+    for (let y = 0; y < rows; y++) {
+        const row = [];
+        for (let x = 0; x < cols; x++) {
+            if (y > 0 && y < rows - 1 && x > 0 && x < cols - 1) {
+                row.push(emptyChar);
+            } else {
+                row.push(wallChar);
+            }
+        }
+        grid.push(row);
+    }
+    return grid;
+}
+
+function replaceCoordinate(grid, y, x, newChar) {
+    grid[y][x] = newChar;
+}
+
+grid = createGrid(between(10, 25), between(20, 50));
+replaceCoordinate(grid, between(1, grid.length), between(1, grid[0].length), playerChar);
+
+// Generate the HTML content for the map
+const mapContent = grid.map(row => row.join('')).join('\n');
+mapArea.innerHTML = mapContent;
